6be49972460cdca1d05615a1977f6f59ba4697ef
Anup Rai <raileohang@gmail.com>
2024-12-12 17:53:17 +0545

    feat: use constant collection names; specify collection name in the mongoose module injection

---
+++ src/schemas/consts.ts
@@ -0,0 +1,4 @@
+export const COLLECTION_NAMES = {
+  USERS: 'users',
+  USER_SETTINGS: 'user_settings',
+};
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { COLLECTION_NAMES } from 'src/schemas/consts';
 import {
   UserSettings,
   UserSettingsSchema,
@@ -14,10 +15,12 @@ import { UsersService } from './users.service';
       {
         name: User.name,
         schema: UserSchema,
+        collection: COLLECTION_NAMES.USERS,
       },
       {
         name: UserSettings.name,
         schema: UserSettingsSchema,
+        collection: COLLECTION_NAMES.USER_SETTINGS,
       },
     ]),
   ],
