47be526e7a005a433373f21fd5c3a3c4dee66018
Anup Rai <raileohang@gmail.com>
2024-12-12 10:30:33 +0545

    chore: setup nestjs/config with yaml parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { readFileSync } from 'fs';
+import { load as yamlLoad } from 'js-yaml';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [() => yamlLoad(readFileSync('config/default.yml', 'utf-8'))],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('database.mongo_url'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
