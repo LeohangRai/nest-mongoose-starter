d8e02f7bb17946dffd2385467eceac528f100c80
Anup Rai <raileohang@gmail.com>
2024-12-13 22:22:27 +0545

    docs: setup swagger docs

---
+++ src/common/helpers/swagger/swagger-config.ts
@@ -0,0 +1,14 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const SWAGGER_URL = '/api-docs';
+
+export const SWAGGER_CONFIG = new DocumentBuilder()
+  .setTitle('NestJS Mongoose API')
+  .setDescription('OpenAPI documentation for NestJS Mongoose API')
+  .setVersion('1.0')
+  .addTag('nestjs-mongoose-api')
+  .addBearerAuth({
+    type: 'http',
+    scheme: 'bearer',
+  })
+  .build();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
+import {
+  SWAGGER_CONFIG,
+  SWAGGER_URL,
+} from './common/helpers/swagger/swagger-config';
 import { formatErrors } from './common/helpers/validation/format-errors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -16,6 +25,25 @@ async function bootstrap() {
       },
     }),
   );
+
+  /* set basic auth (prompt) for Swagger docs */
+  const swaggerUser = configService.get<string>('swagger.user');
+  const swaggerPassword = configService.get<string>('swagger.password');
+  if (swaggerUser && swaggerPassword) {
+    app.use(
+      [SWAGGER_URL],
+      basicAuth({
+        challenge: true,
+        users: { [swaggerUser]: swaggerPassword },
+      }),
+    );
+  }
+
+  /* enable Swagger docs */
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, SWAGGER_CONFIG);
+  SwaggerModule.setup(SWAGGER_URL, app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
